72347d18d00be34ad649f70354085f8880c4eb59
jae <kjlee.ko@gmail.com>
2020-05-06 01:02:19 +0900

    statistics 부분 복구

--- docker/ops-playground-image/java/flink-playground-clickcountjob/src/main/java/org/apache/flink/playgrounds/ops/clickcount/ClickEventCount.java
+++ docker/ops-playground-image/java/flink-playground-clickcountjob/src/main/java/org/apache/flink/playgrounds/ops/clickcount/ClickEventCount.java
@@ -6,8 +6,6 @@ import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.playgrounds.ops.clickcount.functions.BackpressureMap;
 import org.apache.flink.playgrounds.ops.clickcount.functions.ClickEventStatisticsCollector;
 import org.apache.flink.playgrounds.ops.clickcount.functions.CountingAggregator;
-import org.apache.flink.playgrounds.ops.clickcount.functions.MyMetricCounter;
-import org.apache.flink.playgrounds.ops.clickcount.functions.MyMetricHistogram;
 import org.apache.flink.playgrounds.ops.clickcount.records.ClickEvent;
 import org.apache.flink.playgrounds.ops.clickcount.records.ClickEventDeserializationSchema;
 import org.apache.flink.playgrounds.ops.clickcount.records.ClickEventSerializationSchema;
@@ -23,7 +21,6 @@ import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer.Semantic;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.StringSerializer;
 
 /**
  * A simple streaming job reading {@link ClickEvent}s from Kafka, counting events per 15 seconds and
@@ -78,6 +75,7 @@ public class ClickEventCount {
     if (eventTimeSemantics) {
       env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
     }
+
     //disabling Operator chaining to make it easier to follow the Job in the WebUI
     env.disableOperatorChaining();
 
@@ -101,7 +99,6 @@ public class ClickEventCount {
 
     DataStream<ClickEvent> clicks = env.addSource(
         new FlinkKafkaConsumer<>(inputTopic, new ClickEventDeserializationSchema(), kafkaProps))
-        .name("ClickEvent Source")
         .assignTimestampsAndWatermarks(
             new BoundedOutOfOrdernessTimestampExtractor<ClickEvent>(
                 Time.of(200, TimeUnit.MILLISECONDS)) {
@@ -110,8 +107,9 @@ public class ClickEventCount {
                 return element.getTimestamp().getTime();
               }
             })
-        .map(new MyMetricCounter())
-        .map(new MyMetricHistogram());
+//        .map(new MyMetricCounter())
+//        .map(new MyMetricHistogram())
+        .name("ClickEvent Source");
 
     if (this.inflictBackpressure) {
       // Force a network shuffle so that the backpressure will affect the buffer pools
@@ -127,18 +125,18 @@ public class ClickEventCount {
         Semantic.EXACTLY_ONCE))
         .name("ClickEvent Sink");
 
-//    DataStream<ClickEventStatistics> statistics = clicks
-//        .keyBy(ClickEvent::getPage)
-//        .timeWindow(WINDOW_SIZE)
-//        .aggregate(new CountingAggregator(), new ClickEventStatisticsCollector())
-//        .name("ClickEvent Counter");
-//
-//    statistics.addSink(new FlinkKafkaProducer<>(
-//        statisticsTopic,
-//        new ClickEventStatisticsSerializationSchema(statisticsTopic),
-//        kafkaProps,
-//        FlinkKafkaProducer.Semantic.AT_LEAST_ONCE))
-//        .name("ClickEventStatistics Sink");
+    DataStream<ClickEventStatistics> statistics = clicks
+        .keyBy(ClickEvent::getPage)
+        .timeWindow(WINDOW_SIZE)
+        .aggregate(new CountingAggregator(), new ClickEventStatisticsCollector())
+        .name("ClickEvent Counter");
+
+    statistics.addSink(new FlinkKafkaProducer<>(
+        statisticsTopic,
+        new ClickEventStatisticsSerializationSchema(statisticsTopic),
+        kafkaProps,
+        FlinkKafkaProducer.Semantic.EXACTLY_ONCE))
+        .name("ClickEventStatistics Sink");
 
     env.execute("Click Event Count");
   }
